f5f6d57891d4f5a9c67b50cd0c90eaf608fdff94
Shreya <shreyapandey06@gmail.com>
2023-04-28 18:15:59 +0530

    done

--- main.py
+++ main.py
@@ -130,7 +130,7 @@ translator = Translator()
 app.config['MAIL_SERVER']='smtp.gmail.com'
 app.config['MAIL_PORT'] = 465
 app.config['MAIL_USERNAME'] = 'newsinproject@gmail.com'
-app.config['MAIL_PASSWORD'] = 'cmhiudhkeeaaphut'
+app.config['MAIL_PASSWORD'] = 'pdcqacbeaffacwcp'
 app.config['MAIL_USE_TLS'] = False
 app.config['MAIL_USE_SSL'] = True
 
